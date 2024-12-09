46a3f78dfe806edbb6f95a14203e193fb1b29778
Jonas <jonysbina@gmail.com>
2024-12-09 16:41:01 +0100

    day 9 part 2 wtf

--- src/main/kotlin/solutions/Day09.kt
+++ src/main/kotlin/solutions/Day09.kt
@@ -13,34 +13,34 @@ fun main() {
 class Day09(
     override val inputs: Inputs
 ) : Day(inputs) {
-    var string = ""
+    var string = mutableListOf<String>()
     override fun part1(test: Boolean): Any {
         val input = (if (test) inputs.testInput else inputs.input).input
         var file = true
         var i = 0
         var id = 0
-        string = ""
+        string = mutableListOf()
         println("Reading input")
         input.forEach { c ->
             val digit = c.digitToInt()
             if (file) {
                 for (x in 0..<digit) {
-                    string += id
+                    string.add(id.toString())
                 }
                 id++
             } else {
                 for (x in 0..<digit) {
-                    string += '.'
+                    string.add(".")
                 }
             }
             i += digit
             file = !file
         }
-
-        val ca = string.toCharArray()
         println("assembling")
-        var dots = ca.mapIndexed { index, c -> c to index }.filter { it.first == '.' }.map { it.second }.toMutableList()
-        var numbers = ca.mapIndexed { index, c -> c to index }.filter { it.first.isDigit() }.reversed().toMutableList()
+        var dots = string.mapIndexed { index, c -> c to index }.filter { it.first.first() == '.' }.map { it.second }
+            .toMutableList()
+        var numbers = string.mapIndexed { index, c -> c to index }.filter { it.first.first().isDigit() }.reversed()
+            .toMutableList()
         while (true) {
             val d = dots.first()
             val n = numbers.first()
@@ -55,20 +55,96 @@ class Day09(
                 numbers.sortByDescending {
                     it.second
                 }
-                ca[d] = n.first
-                ca[n.second] = '.'
+                string[d] = n.first
+                string[n.second] = "."
             }
         }
         println("calculating")
         var sum = 0L
-        ca.filter { it.isDigit() }.forEachIndexed { ind, c ->
-            sum+=c.digitToInt() * ind
+        string.filter { it.first().isDigit() }.forEachIndexed { ind, c ->
+            sum += c.toInt() * ind
         }
         return sum
     }
 
     override fun part2(test: Boolean): Any {
-        val input = if (test) inputs.testInput else inputs.input
-        return 0
+        val input = (if (test) inputs.testInput else inputs.input).input
+        val clusters = mutableListOf<List<Pair<String, Int>>>()
+        var cluster = mutableListOf<Pair<String, Int>>()
+        var file = true
+        var i = 0
+        var id = 0
+        string = mutableListOf()
+        println("Reading input")
+        input.forEach { c ->
+            val digit = c.digitToInt()
+            if (file) {
+                for (x in 0..<digit) {
+                    string.add(id.toString())
+                }
+                id++
+            } else {
+                for (x in 0..<digit) {
+                    string.add(".")
+                }
+            }
+            i += digit
+            file = !file
+        }
+
+        string.forEachIndexed { i, it ->
+            if (cluster.isEmpty()) {
+                cluster.add(it to i)
+            } else {
+                if (cluster[0].first != it) {
+                    clusters.add(cluster)
+                    cluster = mutableListOf(it to i)
+                } else {
+                    cluster.add(it to i)
+                }
+            }
+        }
+        var dotClusters =
+            clusters.mapIndexed { index, c -> c to index }.filter { it.first.any { it.first.first() == '.' } }
+                .toMutableList()
+        var numberClusters =
+            clusters.mapIndexed { index, c -> c to index }.filter { it.first.any { it.first.first().isDigit() } }
+                .reversed().toMutableList()
+
+        while (true) {
+            val d = dotClusters.first()
+            if (numberClusters.none { ncluster -> ncluster.first.joinToString("") { it.first }.length <= d.first.size }) {
+                break
+            } else {
+                val n = numberClusters.first { ncluster -> ncluster.first.joinToString("") { it.first }.length <= d.first.size }
+
+
+                numberClusters.remove(n)
+                numberClusters.add(n.first to d.second)
+                dotClusters.remove(d)
+                dotClusters.add(d.first to n.second)
+                dotClusters.sortBy {
+                    it.second
+                }
+                numberClusters.sortByDescending {
+                    it.second
+                }
+                val dots = d.first
+                testPrintln(string.toString(),test)
+                n.first.forEachIndexed { index, c ->
+                    val dot = dots[index]
+                    string[dot.second]=c.first
+                    string[c.second]="."
+                }
+                testPrintln(string.toString(),test)
+            }
+        }
+        println("calculating")
+        var sum = 0L
+        testPrintln(string.toString(), test)
+        string.filter { it.first().isDigit() }.forEachIndexed { ind, c ->
+            sum += c.toInt() * ind
+        }
+        return sum
     }
 }
